282ab364f4f5f1bd35a66b4f490f93f96296bebf
maxbbn <qipbbn@gmail.com>
2011-11-04 15:34:02 +0800

    update shareset list view

---
+++ public/js/rate/tags.js
@@ -0,0 +1,15 @@
+KISSY.add('rate/tags',function(S){
+    var $ = S.all;
+
+    var temp = S.one('#temp-tags')[0].innerHTML;
+    var tagsTmpl = new S.Template(temp);
+
+    S.io({
+        url : '/json/tags',
+        dataType : 'json',
+        type : 'get',
+        success : function(data){
+            $('#tags .bd').html(tagsTmpl.render(data));
+        }
+    });
+});
