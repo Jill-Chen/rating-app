9b9d003604239dc3866dd2dc2668ffdfa1a4e286
unknown <bengle@bengle-PC.(none)>
2012-01-18 15:53:12 +0800

    test test

--- public/js/feedback/feedTable.js
+++ public/js/feedback/feedTable.js
@@ -17,20 +17,6 @@ KISSY.ready(function(S) {
                 DOM.hide(e.currentTarget);
             }
             DOM.css('.ks-switchable-content','height','590px');
-        });
-        Event.on('#scroller-prev','click',function(e){
-            if(current>=0){
-                DOM.show(papers[current-1]);
-                current--;
-            }
-            if(current != end-1){
-                DOM.hide('#submit-form');
-                DOM.show('#scroller-next');
-            }
-            if(current == 0){
-                DOM.hide('#scroller-prev');
-                DOM.show('#submit-back');	
-                DOM.css('.ks-switchable-content','height','400px');
-            }
+			
         });
     });
