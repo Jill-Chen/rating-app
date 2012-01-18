c8a363f51a62ae46e4436c4c1cb3d7845a360422
unknown <bengle@bengle-PC.(none)>
2012-01-18 17:27:11 +0800

    validate and switchable nav add validate and switchable nav js.

--- public/js/feedback/feedTable.js
+++ public/js/feedback/feedTable.js
@@ -1,22 +1,44 @@
-KISSY.ready(function(S) {
+KISSY.use('sizzle',function(S){	
+	S.ready(function() {
         var DOM = S.DOM,Event = S.Event;
         var papers = DOM.query('fieldset');
         var current = 0,end = papers.length;
-        DOM.hide('#submit-form');
-        DOM.hide('#scroller-prev');
-        DOM.css('.ks-switchable-content','height','400px');
-        Event.on('#scroller-next','click',function(e){
-            if(current < end){
-                DOM.hide(papers[current]);
-                current++;
-                DOM.show('#scroller-prev');
-                DOM.hide('#submit-back');
-            }
-            if(current == end-1){
-                DOM.show('#submit-form');
-                DOM.hide(e.currentTarget);
-            }
-            DOM.css('.ks-switchable-content','height','590px');
-			
-        });
+		var createNav = function(navNum){
+			for(var j=0;j<navNum;j++){
+				DOM.append('<li>'+j+'</li>','ul.switch-nav');
+			}
+			DOM.css(DOM.children('ul.switch-nav')[0],'background','red');
+		}
+		var changeNav = function(current){
+			DOM.css(DOM.children('ul.switch-nav'),'background','#000');
+			DOM.css(DOM.children('ul.switch-nav')[current],'background','red');
+		}
+		var initPage = function(){
+			DOM.hide('#submit-form');
+			DOM.hide('#scroller-prev');
+			DOM.css('.ks-switchable-content','height','400px');
+			createNav(papers.length);
+			Event.on('#scroller-next','click',function(e){
+				for(var i=0;i<options.length;i++){
+					if(DOM.query('.ctl-score:eq('+i+')>:checkd') == 0){
+						DOM.css(DOM.children(papers[i])[0],'color','red');
+						return;
+					}
+				}
+				if(current < end){
+					DOM.hide(papers[current]);
+					current++;
+					DOM.show('#scroller-prev');
+					DOM.hide('#submit-back');
+				}
+				if(current == end-1){
+					DOM.show('#submit-form');
+					DOM.hide(e.currentTarget);
+				}
+				DOM.css('.ks-switchable-content','height','590px');
+				changeNav(current);
+			});
+		}
+		initPage();
     });
+});
