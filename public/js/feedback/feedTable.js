fcc81655f5767fb7c2b7ff8a628bc78e114ef079
qiyou <qiyou.xj@taobao.com>
2012-02-14 17:01:20 +0800

    test

--- public/js/feedback/feedTable.js
+++ public/js/feedback/feedTable.js
@@ -1,43 +1,73 @@
 KISSY.use('sizzle',function(S){	
 	S.ready(function() {
-        var DOM = S.DOM,Event = S.Event;
-        var papers = DOM.query('fieldset');
+        var DOM = S.DOM,Event = S.Event,$ = S.all;
+        var papers = $('fieldset');
         var current = 0,end = papers.length;
+        var optionLength = 10,optionSize = [2,4];
+
 		var createNav = function(navNum){
-			for(var j=0;j<navNum;j++){
-				DOM.append('ul.switch-nav','<li>'+j+'</li>');
+			DOM.hide('#submit-form');
+			DOM.hide('#scroller-prev');
+			DOM.css('.ks-switchable-content','height','447px');
+            for(var j=0;j<navNum;j++){
+				$('ul.switch-nav').append('<li>'+j+'</li>');
 			}
 			DOM.css(DOM.children('ul.switch-nav')[0],'background','red');
 		}
+
 		var changeNav = function(current){
 			DOM.css(DOM.children('ul.switch-nav'),'background','#000');
 			DOM.css(DOM.children('ul.switch-nav')[current],'background','red');
 		}
+
+        var optCheck = function(current){
+            var chk = $('fieldset:eq('+current+') :checked').length,
+                baseline = $('fieldset:eq('+current+') .ctl-score').length;
+            console.log(current);
+            if(current == 0){
+                if(chk<optionSize[0]){
+                    for(var i=0;i<baseline;i++){
+                        if($('fieldset:eq('+current+') .ctl-score:eq('+i+') :checked').length == 0){
+                            DOM.css($('fieldset:eq('+current+') .clearfix:eq('+i+') label'),'color','red');
+                        }
+                    }
+                    return;
+                } 
+            }else {
+                if(chk<optionSize[1]){
+                    for(var i=0;i<baseline;i++){
+                        if($('fieldset:eq('+current+') .ctl-score:eq('+i+') :checked').length == 0){
+                            DOM.css($('fieldset:eq('+current+') .clearfix:eq('+i+') label'),'color','red');
+                        }
+                    }
+                    return;
+                }
+            }
+            if(current < end){
+                DOM.hide(papers[current]);
+                current++;
+            }
+            if(current == end-1){
+                DOM.show('#submit-form');
+                DOM.hide('#scroller-next');
+            }
+            if(current == end){
+                $('form.feedback').submit();
+            }
+
+            DOM.css('.ks-switchable-content','height','640px');
+        }
+
 		var initPage = function(listNum){
-			DOM.hide('#submit-form');
-			DOM.hide('#scroller-prev');
-			DOM.css('.ks-switchable-content','height','400px');
 			createNav(listNum);
 			Event.on('#scroller-next','click',function(e){
-				for(var i=0;i<options.length;i++){
-					if(DOM.query('.ctl-score:eq('+i+')>:checkd') == 0){
-						DOM.css(DOM.children(papers[i])[0],'color','red');
-						return;
-					}
-				}
-				if(current < end){
-					DOM.hide(papers[current]);
-					current++;
-					DOM.show('#scroller-prev');
-					DOM.hide('#submit-back');
-				}
-				if(current == end-1){
-					DOM.show('#submit-form');
-					DOM.hide(e.currentTarget);
-				}
-				DOM.css('.ks-switchable-content','height','590px');
+				optCheck(current);
 				changeNav(current);
 			});
+            Event.on('#submit-form','click',function(e){
+                e.preventDefault();
+                optCheck(end);
+            });
 		}
 		initPage(end);
     });
